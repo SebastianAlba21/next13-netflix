379d0e4b8d971e93aa2c79a9921f13d51dd82ecf
Sebastián Alba <juan.alba@uao.edu.co>
2023-04-03 20:01:18 -0500

    Pagina de inicio full UI

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,17 +1,35 @@
-const Input = () => {
+import React from "react";
+
+interface InputProps {
+  id: string;
+  onChange: any;
+  value: string;
+  label: string;
+  type?: string;
+}
+
+const Input: React.FC<InputProps> = ({
+  id,
+  onChange,
+  value,
+  label,
+  type = "text",
+}) => {
   return (
     <div className="relative">
       <input
-        id="email"
-        type="text"
+        onChange={onChange}
+        id={id}
+        value={value}
+        type={type}
         className="block rounded-md px-6 pt-6 pb-1 w-full text-md text-white bg-neutral-700 appearance-none foucs:outline-none focus:ring-0 peer"
         placeholder=" "
       />
       <label
-        htmlFor="email"
-        className="absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-3 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-1 peer-focus:scale-75 peer-focus:-translate-y-3"
+        htmlFor={id}
+        className="absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-2 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-1 peer-focus:scale-75 peer-focus:-translate-y-3"
       >
-        Email
+        {label}
       </label>
     </div>
   );
