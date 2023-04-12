f51adef2dd640bb342cd99f2719202a6dc8a715f
Sebastián Alba <juan.alba@uao.edu.co>
2023-04-12 11:09:27 -0500

    Se realiza el SignIn de la pagina

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,31 @@
+import { NextPageContext } from "next";
+import { signOut, getSession } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
-  return <h1>Netflix clone</h1>;
+  return (
+    <div className="grid place-content-center gap-5 mt-64">
+      <h1 className="text-4xl">Netflix clone</h1>
+      <button
+        className="h-10 w-[100px] bg-slate-500 rounded-full"
+        onClick={() => signOut()}
+      >
+        Logout!
+      </button>
+    </div>
+  );
 }
